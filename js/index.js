a3068a2049982f990163bc7fe4c9b0841e8094e5
Kauã Yanase <kauayuuki10@gmail.com>
2025-10-10 15:00:17 -0300

    adiciona estrutura HTML, estilização e JS para section produtos

--- js/index.js
+++ js/index.js
@@ -12,3 +12,30 @@ window.addEventListener('scroll', () => {
   }
   lastScrollY = window.scrollY;
 })
+
+/*section-produtos*/
+// Função para deslizar os carrosséis
+document.querySelectorAll('.container-carousel').forEach(carrossel => {
+  const container = carrossel.querySelector('.carousel');
+  const btnEsquerda = carrossel.querySelector('.esquerda');
+  const btnDireita = carrossel.querySelector('.direita');
+
+  btnEsquerda.addEventListener('click', () => {
+    container.scrollBy({ left: -724, behavior: 'smooth' });
+  });
+ 
+  btnDireita.addEventListener('click', () => {
+    container.scrollBy({ left: 724, behavior: 'smooth' });
+  });
+});
+ 
+// Ação do botão "Fazer pedido"
+document.getElementById('fazerPedido').addEventListener('click', () => {
+  alert('Seu pedido foi iniciado!');
+});
+ 
+// ======== BOTÃO VOLTAR AO TOPO ========
+document.getElementById("topBtn").addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+ 
