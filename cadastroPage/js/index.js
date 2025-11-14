9124bc6ba1bc1adfff6ece4bf71a66627ede5704
Kauã Yanase <kauayuuki10@gmail.com>
2025-11-14 00:06:42 -0300

    Nova funcionalidade na pagina de cadastro. Informações do usuário são guardadas localmente, apenas para teste.

---
+++ cadastroPage/js/index.js
@@ -0,0 +1,30 @@
+const nome = document.querySelector('#nome')
+const email = document.querySelector('#email')
+const tel = document.querySelector('#telefone')
+const senha = document.querySelector('#senha')
+const confirmar = document.querySelector('#confirmar-senha')
+
+let usuarios = JSON.parse(localStorage.getItem('usuarios') || [])
+
+document.getElementById('formCadastro').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if(senha.value !== confirmar.value) {
+    alert('As senhas não coincidem')
+    return
+  }
+
+  const novoUsuario = {
+    id: Date.now(),
+    nome: nome.value,
+    email: email.value,
+    telefone: tel.value,
+    senha: senha.value
+  }
+
+  usuarios.push(novoUsuario)
+  localStorage.setItem('usuarios', JSON.stringify(usuarios))
+
+  alert('Cadastro realizado com sucesso')
+});
+
